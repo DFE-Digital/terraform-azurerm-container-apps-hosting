9c0c5c14b72ca380fe4aeb7a3f6bb4521df96897
Chris Wright <chris@dxw.com>
2022-09-13 17:43:56 +0100

    Conditionally create container registry
    * If a container registry doesn't already exist for the app, this can be
      used to store the app images

--- locals.tf
+++ locals.tf
@@ -6,4 +6,5 @@ locals {
   virtual_network_address_space      = var.virtual_network_address_space
   virtual_network_address_space_mask = element(split("/", local.virtual_network_address_space), 1)
   container_apps_infra_subnet_cidr   = cidrsubnet(local.virtual_network_address_space, 23 - local.virtual_network_address_space_mask, 0)
+  enable_container_registry          = var.enable_container_registry
 }
