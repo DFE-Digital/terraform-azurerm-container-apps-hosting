e040ce4cfcfca020134761854872e27a368813b4
Chris Wright <chris@dxw.com>
2022-09-14 09:11:19 +0100

    Conditionally launch Azure SQL
    * Creates an SQL server/database, with a private endpoint within the virtual network
    * Creates a subnet for Container Instances, to allow launching a
      container to access the SQL database

--- locals.tf
+++ locals.tf
@@ -6,10 +6,17 @@ locals {
   virtual_network_address_space      = var.virtual_network_address_space
   virtual_network_address_space_mask = element(split("/", local.virtual_network_address_space), 1)
   container_apps_infra_subnet_cidr   = cidrsubnet(local.virtual_network_address_space, 23 - local.virtual_network_address_space_mask, 0)
+  mssql_private_endpoint_subnet_cidr = cidrsubnet(local.virtual_network_address_space, 23 - local.virtual_network_address_space_mask, 1)
+  container_instances_subnet_cidr    = cidrsubnet(local.virtual_network_address_space, 23 - local.virtual_network_address_space_mask, 2)
   enable_container_registry          = var.enable_container_registry
   registry_server                    = local.enable_container_registry ? azurerm_container_registry.acr[0].login_server : var.registry_server
   registry_username                  = local.enable_container_registry ? azurerm_container_registry.acr[0].admin_username : var.registry_username
   registry_password                  = local.enable_container_registry ? azurerm_container_registry.acr[0].admin_password : var.registry_password
+  enable_mssql_database              = var.enable_mssql_database
+  mssql_server_admin_password        = var.mssql_server_admin_password
+  mssql_sku_name                     = var.mssql_sku_name
+  mssql_max_size_gb                  = var.mssql_max_size_gb
+  mssql_database_name                = var.mssql_database_name
   image_name                         = var.image_name
   image_tag                          = var.image_tag
   container_cpu                      = var.container_cpu
---
+++ mssql.tf
@@ -0,0 +1,79 @@
+resource "azurerm_storage_account" "mssql_security_storage" {
+  count = local.enable_mssql_database ? 1 : 0
+
+  name                     = "${local.resource_prefix}mssqlsec"
+  resource_group_name      = azurerm_resource_group.default.name
+  location                 = azurerm_resource_group.default.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  min_tls_version          = "TLS1_2"
+}
+
+resource "azurerm_mssql_server" "default" {
+  count = local.enable_mssql_database ? 1 : 0
+
+  name                          = local.resource_prefix
+  resource_group_name           = azurerm_resource_group.default.name
+  location                      = azurerm_resource_group.default.location
+  version                       = "12.0"
+  administrator_login           = "${local.resource_prefix}-admin"
+  administrator_login_password  = local.mssql_server_admin_password
+  public_network_access_enabled = false
+  minimum_tls_version           = "1.2"
+}
+
+resource "azurerm_mssql_server_extended_auditing_policy" "default" {
+  count = local.enable_mssql_database ? 1 : 0
+
+  server_id                               = azurerm_mssql_server.default[0].id
+  storage_endpoint                        = azurerm_storage_account.mssql_security_storage[0].primary_blob_endpoint
+  storage_account_access_key              = azurerm_storage_account.mssql_security_storage[0].primary_access_key
+  storage_account_access_key_is_secondary = false
+  retention_in_days                       = 90
+}
+
+resource "azurerm_mssql_database" "default" {
+  count = local.enable_mssql_database ? 1 : 0
+
+  name        = local.mssql_database_name
+  server_id   = azurerm_mssql_server.default[0].id
+  collation   = "SQL_Latin1_General_CP1_CI_AS"
+  sku_name    = local.mssql_sku_name
+  max_size_gb = local.mssql_max_size_gb
+}
+
+resource "azurerm_mssql_database_extended_auditing_policy" "default" {
+  count = local.enable_mssql_database ? 1 : 0
+
+  database_id                             = azurerm_mssql_database.default[0].id
+  storage_endpoint                        = azurerm_storage_account.mssql_security_storage[0].primary_blob_endpoint
+  storage_account_access_key              = azurerm_storage_account.mssql_security_storage[0].primary_access_key
+  storage_account_access_key_is_secondary = false
+  retention_in_days                       = 90
+}
+
+resource "azurerm_private_endpoint" "default_mssql" {
+  count = local.enable_mssql_database ? 1 : 0
+
+  name                = "${local.resource_prefix}defaultmssql"
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
+  subnet_id           = azurerm_subnet.mssql_private_endpoint_subnet[0].id
+
+  private_service_connection {
+    name                           = "${local.resource_prefix}defaultmssqlconnection"
+    private_connection_resource_id = azurerm_mssql_server.default[0].id
+    subresource_names              = ["sqlServer"]
+    is_manual_connection           = false
+  }
+}
+
+resource "azurerm_private_dns_a_record" "mssql_private_endpoint" {
+  count = local.enable_mssql_database ? 1 : 0
+
+  name                = "@"
+  zone_name           = azurerm_private_dns_zone.mssql_private_link[0].name
+  resource_group_name = azurerm_resource_group.default.name
+  ttl                 = 300
+  records             = [azurerm_private_endpoint.default_mssql[0].private_service_connection.0.private_ip_address]
+}
