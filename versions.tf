fe6852f3eed07877e1b245fd9bcd0cf01f16ab1d
Chris Wright <chris@dxw.com>
2022-12-13 15:24:11 +0000

    Bump terraform version v1.3.6
    * Bumps azure/azapi provider to v1.1.0
    * Bumps hashicorp/azurerm provider to v3.35.0

--- versions.tf
+++ versions.tf
@@ -1,13 +1,13 @@
 terraform {
-  required_version = ">= 1.2.9"
+  required_version = ">= 1.3.6"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.32.0"
+      version = ">= 3.35.0"
     }
     azapi = {
       source  = "Azure/azapi"
-      version = ">= 1.0.0"
+      version = ">= 1.1.0"
     }
   }
 }
