602f381e1e3f494893767be3ba84a117998e1cf3
Ash Davies <3853061+DrizzlyOwl@users.noreply.github.com>
2024-01-04 15:37:44 +0000

    Default to using Managed Identity if available
    * If it is unavailable, the sql audit will use access keys as a fallback

--- mssql.tf
+++ mssql.tf
@@ -82,8 +82,6 @@ resource "azurerm_mssql_server_extended_auditing_policy" "default" {
 
   server_id                               = azurerm_mssql_server.default[0].id
   storage_endpoint                        = azurerm_storage_account.mssql_security_storage[0].primary_blob_endpoint
-  storage_account_access_key              = azurerm_storage_account.mssql_security_storage[0].primary_access_key
-  storage_account_access_key_is_secondary = false
   retention_in_days                       = 90
 }
 
@@ -112,8 +110,6 @@ resource "azurerm_mssql_database_extended_auditing_policy" "default" {
 
   database_id                             = azurerm_mssql_database.default[0].id
   storage_endpoint                        = azurerm_storage_account.mssql_security_storage[0].primary_blob_endpoint
-  storage_account_access_key              = azurerm_storage_account.mssql_security_storage[0].primary_access_key
-  storage_account_access_key_is_secondary = false
   retention_in_days                       = 90
 }
 
