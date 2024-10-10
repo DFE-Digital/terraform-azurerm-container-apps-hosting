308a98af76561543f7b4e5d2900df47bf4b670ed
Ash Davies <3853061+DrizzlyOwl@users.noreply.github.com>
2024-10-10 16:39:21 +0100

    Added function binding

--- functions/src/health-api/function_app.py
+++ functions/src/health-api/function_app.py
@@ -10,10 +10,12 @@ import json
 credential = DefaultAzureCredential()
 client = LogsQueryClient(credential)
 
-app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 response_headers = { "Content-Type": "application/json" }
 query = """availabilityResults | take 3 | project location, success"""
 
+app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+
+@app.function_name(name="http_trigger")
 @app.route(route="http_trigger")
 def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
