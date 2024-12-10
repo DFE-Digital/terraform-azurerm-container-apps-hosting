fd386f6f8e3a5cc87c0711be5731e12e829cd6cd
Ash Davies <3853061+DrizzlyOwl@users.noreply.github.com>
2024-12-10 16:08:26 +0000

    Stabilised version constraints (#445)

--- versions.tf
+++ versions.tf
@@ -1,21 +1,25 @@
 terraform {
-  required_version = ">= 1.9.5, < 2.0.0"
+  required_version = "~> 1.9"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 4.0.0, < 5.0.0"
+      version = "~> 4.0"
     }
+
     azapi = {
       source  = "Azure/azapi"
-      version = ">= 1.13.0"
+      version = "~> 1.13"
     }
+
     null = {
       source  = "hashicorp/null"
-      version = ">= 3.2.1"
+      version = "~> 3.2"
     }
+
     archive = {
       source  = "hashicorp/archive"
-      version = ">= 2.6.0"
+      version = "~> 2.6"
     }
   }
 }
