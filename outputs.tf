5ff544b20669dfa48c64f7eba5fe2b292c69d613
rachelgriffiths-al <rachel.griffiths@airelogic.com>
2026-03-19 16:12:22 +0000

    add output for environment id of container
    used in update to the waf shared module for front door private link

--- outputs.tf
+++ outputs.tf
@@ -65,3 +65,13 @@ output "container_app_environment_ingress_ip" {
   description = "Ingress IP address assigned to the Container App environment"
   value       = local.container_app_environment.static_ip_address
 }
+
+output "container_app_environment_id" {
+description = "Id of the used Container App environment - either existing or created"
+  value = (
+    local.existing_container_app_environment.name == ""
+    ? azurerm_container_app_environment.container_app_env[0].id
+    : data.azurerm_container_app_environment.existing_container_app_environment[0].id
+  )
+}
+
